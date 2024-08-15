77637521c8687ccc8858f640ce6783af9e37a6e5
MichaelBidencopeCP <mbidencope3@gmail.com>
2024-08-15 01:45:37 -0400

    fixing config

--- michael-bidencope/src/api.js
+++ michael-bidencope/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://localhost/api/',
 });
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
